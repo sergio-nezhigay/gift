0f5ad3790f4957d853f72e8684e2d9ec6f02e6ef
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-04 17:35:46 +0200

    add accordion

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 function Example() {
-  return <div>Example3</div>;
+  return (
+    <div className="max-w-md bg-blue-100">
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
 }
 
 export default Example;
