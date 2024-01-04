813531e50b622d28e81d2bbf277da397855c367b
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-04 16:12:33 +0200

    test huski1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg"),
+    );
 
-module.exports = nextConfig
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      },
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Example from "@/components/Example";
+import Test from "@/public/assets/icons/test.svg";
+
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container text-green-400">
       <h1 className="text-9xl text-green-400">Gift</h1>
+      <div className="w-[100px] bg-blue-100  ">
+        <Test className="h-8 w-8 fill-red-600 stroke-blue-300 text-current" />
+      </div>
+      <Example />
       <div className="h-14 w-full bg-emerald-100"></div>
     </div>
   );
