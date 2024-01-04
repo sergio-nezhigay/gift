f9e814a36586cb7b760d8161e0fcf5c753f97a1f
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-04 16:57:51 +0200

    test4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import Example from "@/components/Example";
-import Test from "@/public/assets/icons/test.svg";
+import TestIcon from "@/public/assets/icons/test.svg";
 
 export default function Home() {
   return (
-    <div className="container text-green-400">
-      <h1 className="text-9xl text-green-400">Gift</h1>
-      <div className="w-[100px] bg-blue-100  ">
-        <Test className="h-8 w-8 fill-red-600 stroke-blue-300 text-current" />
-      </div>
+    <div className="container bg-green-300">
+      <h1 className="text-9xl text-green-700">Gift</h1>
+      <TestIcon className="h-12 w-12 fill-red-600 stroke-blue-300" />
       <Example />
-      <div className="h-14 w-full bg-emerald-100"></div>
     </div>
   );
 }
