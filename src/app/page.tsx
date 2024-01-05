ec94469a1041d00b0be9838879d591c97d1afe83
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-05 13:18:31 +0200

    add sizes and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import TestIcon from "@/public/assets/icons/test.svg";
 export default function Home() {
   return (
     <div className="container bg-green-300">
-      <h1 className="text-9xl text-green-700">Подарунок</h1>
-      <TestIcon className="h-[100px] w-[100px] stroke-blue-300" />
+      <h1 className="text-2xl leading-25 text-accent">Подарунок</h1>
+      <TestIcon className="h-[100px] w-[100px] stroke-accent" />
       <Example />
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
       center: true,
       padding: {
         DEFAULT: "20px",
+        sm: "20px",
         md: "30px",
         lg: "32px",
       },
@@ -22,12 +23,38 @@ const config: Config = {
       },
     },
     extend: {
+      fontSize: {
+        "7xl": ["64px", "80.64px"],
+        "6xl": ["60px", "75.6px"],
+        "5xl": ["56px", "70.56px"],
+        "4xl": ["48px", "60.48px"],
+        "3xl": ["24px", "29.26px"],
+        "2xl": ["22px", "29.7px"],
+        xl: ["20px", "24.38px"],
+        lg: ["18px", "24.3px"],
+        base: ["16px", "19.5px"],
+        sm: ["14px", "17.07px"],
+      },
+      lineHeight: {
+        "64": "80.64px",
+        "60": "75.6px",
+        "56": "70.56px",
+        "48": "60.48px",
+        "34": "32.4px",
+        "32": "32.16px",
+        "27": "27px",
+        "26": "26.8px",
+        "25": "26.2px",
+        "16": "21.6px",
+      },
       fontFamily: {
         display: ["var(--font-caveat)"],
         sans: ["var(--font-montserrat)"],
       },
       colors: {
         white: "#ffffff",
+        black: "#000000",
+        accent: "#E42C2C",
       },
       screens: {
         sm: "480px",
