efd3dc358941664e32ca2afcf1d369d54562628a
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-05 13:23:37 +0200

    test build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import TestIcon from "@/public/assets/icons/test.svg";
 export default function Home() {
   return (
     <div className="container bg-green-300">
-      <h1 className="text-2xl leading-25 text-accent">Подарунок</h1>
+      <h1 className="text-2xl leading-25 text-accent">Подарунок!</h1>
       <TestIcon className="h-[100px] w-[100px] stroke-accent" />
       <Example />
     </div>
